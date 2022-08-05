828aff92a49afca9ea7791f67506c82ec171f64f
KHC <98343091+HaleyKwok@users.noreply.github.com>
2022-08-05 15:59:09 +0800

    Update oop.py

--- oop.py
+++ oop.py
@@ -25,6 +25,7 @@ print("Total Employee %d" % Employee.empCount)
 
 print('----------------------------------------------------')
 
+# 
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
  
@@ -54,3 +55,24 @@ c.childMethod()      # 调用子类的方法
 c.parentMethod()     # 调用父类方法
 c.setAttr(200)       # 再次调用父类的方法 - 设置属性值
 c.getAttr()          # 再次调用父类的方法 - 获取属性值
+
+
+print('----------------------------------------------------')
+
+# overloading
+#!/usr/bin/python
+ 
+class Vector:
+   def __init__(self, a, b):
+      self.a = a
+      self.b = b
+ 
+   def __str__(self):
+      return 'Vector (%d, %d)' % (self.a, self.b)
+   
+   def __add__(self, other):
+      return Vector(self.a + other.a, self.b + other.b)
+ 
+v1 = Vector(2,10)
+v2 = Vector(5,-2)
+print(v1 + v2)
