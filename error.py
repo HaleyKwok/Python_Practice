0e13adc42ddd8bf99f0f3b066231004cf4febd85
KHC <98343091+HaleyKwok@users.noreply.github.com>
2022-08-05 11:18:41 +0800

    Update error.py

--- error.py
+++ error.py
@@ -1,3 +1,35 @@
+# try/except
+while True: 
+	try: 
+		x = int(input('Please input a number.'))
+		break
+	except ValueError:
+		print('Error!')
+
+print('----------------------------------------------------')
+
+# try/except...else
+import sys
+for arg in sys.argv[1:]:
+    try:
+        fh = open("testfile", "w")
+        fh.write("This is a test file for testing for exceptions!!!")
+    except IOError:
+        print("Error: file not found or failed to read file")
+    else:
+        print("Content was written to file successfully")
+        fh.close()
+
+# try-finally
+try:
+    fh = open("testfile", "w")
+    fh.write("testing")
+finally:
+    print("Error!")
+
+print('----------------------------------------------------')
+
+# func()
 def this_fails():
 	x = 1/0
 try: 
@@ -8,7 +40,9 @@ except ZeroDivisionError as err:
 
 print('----------------------------------------------------')
 
-def functionName( level ):
+
+# raise
+def functionName(level):
     if level < 1:
         raise Exception("Invalid level!", level) # after raising an error, no execution afterwards
 
