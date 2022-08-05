3d1d53950f37265a6825f19c73fae62580a4f189
KHC <98343091+HaleyKwok@users.noreply.github.com>
2022-08-05 15:48:22 +0800

    Update oop.py

--- oop.py
+++ oop.py
@@ -1,5 +1,5 @@
 class Employee:
-   '所有员工的基类'
+   # 所有员工的基类
    empCount = 0
  
    def __init__(self, name, salary):
@@ -13,10 +13,44 @@ class Employee:
    def displayEmployee(self):
       print("Name : ", self.name,  ", Salary: ", self.salary)
  
-"创建 Employee 类的第一个对象"
+# 创建 Employee 类的第一个对象
 emp1 = Employee("Zara", 2000)
-"创建 Employee 类的第二个对象"
+# 创建 Employee 类的第二个对象
 emp2 = Employee("Manni", 5000)
+
+# 使用点号 . 来访问对象的属性。使用如下类的名称访问类变量：
 emp1.displayEmployee()
 emp2.displayEmployee()
 print("Total Employee %d" % Employee.empCount)
+
+print('----------------------------------------------------')
+
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+ 
+class Parent:        # 定义父类
+   parentAttr = 100
+   def __init__(self):
+      print("调用父类构造函数")
+ 
+   def parentMethod(self):
+      print('调用父类方法')
+ 
+   def setAttr(self, attr):
+      Parent.parentAttr = attr
+ 
+   def getAttr(self):
+      print("父类属性 :", Parent.parentAttr)
+ 
+class Child(Parent): # 定义子类
+   def __init__(self):
+      print("调用子类构造方法")
+ 
+   def childMethod(self):
+      print('调用子类方法')
+ 
+c = Child()          # 实例化子类
+c.childMethod()      # 调用子类的方法
+c.parentMethod()     # 调用父类方法
+c.setAttr(200)       # 再次调用父类的方法 - 设置属性值
+c.getAttr()          # 再次调用父类的方法 - 获取属性值
