2836839f64a8a3a0fec2cd7daab465eb6ab1e6fb
KHC <98343091+HaleyKwok@users.noreply.github.com>
2022-08-06 20:58:41 +0800

    Update functions.py

--- Exercises/functions.py
+++ Exercises/functions.py
@@ -1,17 +1,19 @@
 # function body
-def calculate_total(exp):  #function name; input  -> function arguments
+def calculate_total(exp): #function name; input  -> function arguments
     total = 0
     for i in exp: #loop body
         total = total + i 
     return total #return value
 
 
-
 tom_exp_list = [2100,3400,3500]
 joe_exp_list = [200,500,700]
 
-toms_total = calculate_total(tom_exp_list)
+tom_total = calculate_total(tom_exp_list)
 joe_total = calculate_total(joe_exp_list)
 
-print("Tom's total expenses:", toms_total)
+print("Tom's total expenses:", tom_total)
 print("Joe's total expenses:", joe_total)
+
+
+print('------------------------------------')
